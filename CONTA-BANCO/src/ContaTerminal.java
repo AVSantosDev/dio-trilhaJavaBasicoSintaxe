20ca459ee395829f461480d67cf9d2479720dab4
Andre.vsantos <andrev0795@hotmail.com>
2024-06-04 22:28:19 -0300

    Desafio banco

---
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o numero da Agencia");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor digite o numero da Conta");
+        String numeroConta = scanner.nextLine();
+
+        System.out.println("Por favor digite o  Nome do cliente");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor digite Saldo");
+        String saldo = scanner.nextLine();
+
+        String mensagem = "Ola" + nomeCliente + ", Obrigado por criar uma conta com o nosso banco sua Agencia e " + agencia + " sua Conta e " + numeroConta + " eu saldo e " + saldo + " Disponivel para saque";
+
+        System.err.println(mensagem);
+        scanner.close();
+
+    }
+
+}
